de6780cd2ad3ee17e2ac982b9692b66839f43488
ntovarsolorzano <55255998+ntovarsolorzano@users.noreply.github.com>
2022-03-12 21:52:17 -0500

    Update easy-samba-installer.sh

--- easy-samba-installer.sh
+++ easy-samba-installer.sh
@@ -5,7 +5,18 @@ echo "1) Install Packets"
 sudo apt install samba smbclient vim nano -y
 sudo mkdir -p /mnt/samba/{Public,Private}
 
-echo "2) Set access to folders"
+echo "2) Creating an username, group, and setting prmissions"
+sudo addgroup smbgroup
+echo "Write the username of samba admin"
+read USER
+sudo useradd -s /usr/sbin/nologin $USER
+sudo smbpasswd -a $USER
+sudo usermod -aG smbgroup $USER
+sudo chown root:smbgroup /svr/samba/
+sudo chmod g+w /svr/samba/
+sudo chmod o+wwx /svr/samba/Public
+
+echo "3) Set access to folders"
 echo "[Public]
    comment = Publico 
    path = /svr/samba/Public
@@ -23,15 +34,6 @@ echo "[Private]
    #valid users = $USER
    guest ok = no " | sudo tee -a /etc/samba/smb.conf
 
-echo "3) Creating an username, group, and setting prmissions"
-sudo addgroup smbgroup
-echo "Write the username of samba admin"
-read USER
-sudo useradd -s /usr/sbin/nologin $USER
-sudo smbpasswd -a $USER
-sudo usermod -aG smbgroup $USER
-sudo chown root:smbgroup /svr/samba/
 sudo systemctl restart smbd.service
-sudo chmod g+w /svr/samba/
-sudo chmod o+wwx /svr/samba/Public
+sudo systemctl restart nmbd.service
 echo "Installation finished! Horray! :D"
