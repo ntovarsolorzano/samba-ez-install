b3f385eeef9078fa6f1540e696d41f37fefa61c9
ntovarsolorzano <55255998+ntovarsolorzano@users.noreply.github.com>
2022-07-14 16:05:51 -0500

    Update easy-samba-installer.sh

--- easy-samba-installer.sh
+++ easy-samba-installer.sh
@@ -8,7 +8,7 @@ sudo mkdir -p /svr/samba/{Public,Private}
 echo "2) Creating an username, group, and setting prmissions"
 sudo addgroup smbgroup
 echo "Write the username of samba admin"; read USER
-sudo useradd -s /usr/sbin/nologin $USER
+sudo useradd -aG /usr/sbin/nologin $USER
 sudo smbpasswd -a $USER
 sudo usermod -aG smbgroup $USER
 sudo chown root:smbgroup /svr/samba/
